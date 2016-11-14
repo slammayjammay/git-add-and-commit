d8cbc5a94606dcf0f8671f08434c5f1bcc2e0cca
Scott McKenzie <slammayjammay@gmail.com>
2016-11-13 17:59:31 -0800

    Make non-interactive version work

--- src/index.js
+++ src/index.js
@@ -1,13 +1,31 @@
+var execSync = require('child_process').execSync;
 var Interactive = require('./interactive');
 
 function GitAddAndCommit(options) {
 	if (options.help) {
 		console.log('help screen');
-	}
-
-	if (options.interactive) {
+	} else if (options.interactive) {
 		new Interactive().run();
+	} else {
+		this.normal();
 	}
 }
+
+var proto = GitAddAndCommit.prototype;
+
+proto.normal = function() {
+	try {
+		var args = process.argv.slice(2);
+		var fileGlob = args[0];
+		var commitMessage = args[1];
+
+		execSync('git add -- *' + fileGlob + '*');
+		execSync('git commit -m "' + commitMessage + '"')
+	} catch (e) {
+		console.log(e);
+		console.log('Resetting added files...');
+		execSync('git reset .');
+	}
+};
 
 module.exports = GitAddAndCommit;
