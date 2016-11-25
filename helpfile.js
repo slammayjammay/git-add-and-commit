2e9308e526421a086d3a5759567630ef857dc2a1
Scott McKenzie <slammayjammay@gmail.com>
2016-11-25 15:08:01 -0800

    Add helpfile

--- helpfile.js
+++ helpfile.js
@@ -1,16 +1,25 @@
-[1mDESCRIPTION[22m
+const execSync = require('child_process').execSync
+const chalk = require('chalk')
+
+const outputFile = 'src/help.txt'
+
+let txt = `\
+${chalk.bold('DESCRIPTION')}
   This command quickens the commit process for individual files. Files can be
   added by giving globs, or substrings of the desired file. For example, the
   glob "ind" would find all files that contain "ind" in them.
 
-[1mUSAGE[22m
+${chalk.bold('USAGE')}
   gac [options]
   gac <path> <message>
 
-[1mOPTIONS[22m
+${chalk.bold('OPTIONS')}
   -h, --help          Display this help screen.
   -i, --interactive   Interactively find and commit files.
 
-[1mEXAMPLES[22m
+${chalk.bold('EXAMPLES')}
   gac README.md "Add README"
-  gac index "Commit all index files"
+  gac ind "Modify index.js"
+`
+
+execSync('echo "' + txt + '" > ' + outputFile)
