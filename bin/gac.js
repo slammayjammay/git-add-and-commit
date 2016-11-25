7c67e68b1dddbe14aa76eb551ca2d5fef17dafb1
Scott McKenzie <slammayjammay@gmail.com>
2016-11-25 15:07:36 -0800

    Add bin/

---
+++ bin/gac.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+'use strict'
+
+const join = require('path').join
+const GitAddAndCommit = require(join(__dirname, '../src/git-add-and-commit'))
+
+let options = {}
+let args = process.argv.slice(2)
+
+if (args.includes('-i') || args.includes('--interactive')) {
+	options.interactive = true
+}
+if (args.includes('-h') || args.includes('--help')) {
+	options.help = true
+}
+
+new GitAddAndCommit(options)
