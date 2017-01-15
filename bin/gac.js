aa91856ed1a1844edf3802ec9950d36a7732d764
Scott McKenzie <slammayjammay@gmail.com>
2017-01-14 21:02:50 -0800

    Use minimist

--- bin/gac.js
+++ bin/gac.js
@@ -2,16 +2,22 @@
 'use strict'
 
 const join = require('path').join
+const minimist = require('minimist')
 const GitAddAndCommit = require(join(__dirname, '../src/git-add-and-commit'))
 
-let options = {}
-let args = process.argv.slice(2)
+let args = Object.keys(minimist(process.argv.slice(2)))
+let options = {
+	caseInsensitive: true
+}
 
-if (args.includes('-i') || args.includes('--interactive')) {
+if (args.includes('h') || args.includes('help')) {
+	options.help = true
+}
+if (args.includes('i') || args.includes('interactive')) {
 	options.interactive = true
 }
-if (args.includes('-h') || args.includes('--help')) {
-	options.help = true
+if (args.includes('c') || args.includes('case-sensitive')) {
+	options.caseInsensitive = false
 }
 
 new GitAddAndCommit(options)
