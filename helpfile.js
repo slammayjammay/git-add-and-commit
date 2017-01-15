f4d64eb43e6171f1045d5abe259e53ebd02b901b
Scott McKenzie <slammayjammay@gmail.com>
2017-01-14 21:06:30 -0800

    Update help screen

--- helpfile.js
+++ helpfile.js
@@ -7,19 +7,21 @@ let txt = `\
 ${chalk.bold('DESCRIPTION')}
   This command quickens the commit process for individual files. Files can be
   added by giving globs, or substrings of the desired file. For example, the
-  glob "ind" would find all files that contain "ind" in them.
+  glob "ind" would find all files that contain "ind" in them. Files will be
+  matched case-insensitively by default.
 
 ${chalk.bold('USAGE')}
   gac [options]
   gac <path> <message>
 
 ${chalk.bold('OPTIONS')}
-  -h, --help          Display this help screen.
-  -i, --interactive   Interactively find and commit files.
+  -c, --case-sensitive    Match files case-sensitively.
+  -h, --help              Display this help screen.
+  -i, --interactive       Interactively find and commit files.
 
 ${chalk.bold('EXAMPLES')}
   gac README.md "Add README"
   gac ind "Modify index.js"
 `
 
-execSync('echo "' + txt + '" > ' + outputFile)
+execSync(`echo "${txt}" > ${outputFile}`)
