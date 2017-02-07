cc98cc760cd3a04b649ab1ff64e5bfdb7a2c3a7c
slammayjammay <slammayjammay@gmail.com>
2017-02-07 12:50:29 -0800

    Add --only and --except options for which git file types to look for

--- src/bin/gac.js
+++ src/bin/gac.js
@@ -5,21 +5,64 @@ const join = require('path').join
 const minimist = require('minimist')
 const GitAddAndCommit = require(join(__dirname, '../'))
 
-const CLI_OPTIONS = ['caseSensitive', 'help', 'interactive']
+let booleans = {
+	c: 'caseSensitive',
+	h: 'help',
+	i: 'interactive'
+}
+
+let needArgs = {
+	e: 'except',
+	o: 'only'
+}
+
+const CLI_OPTIONS = Object.assign({}, booleans, needArgs)
 
 let argv = minimist(process.argv.slice(2), {
-	alias: {
-		c: 'caseSensitive',
-		h: 'help',
-		i: 'interactive',
-		'case-sensitive': 'caseSensitive'
-	},
-	boolean: CLI_OPTIONS
+	alias: CLI_OPTIONS,
+	boolean: Object.keys(booleans).map(key => booleans[key])
 })
 
 let options = {}
-for (let option of CLI_OPTIONS) {
-	options[option] = argv[option]
+setupOptions(options)
+parseFilesToFind(options)
+new GitAddAndCommit(options)
+
+//
+// Helpers
+//
+function setupOptions(options) {
+	for (let option of Object.keys(booleans).map(key => booleans[key])) {
+		options[option] = argv[option]
+	}
 }
 
-new GitAddAndCommit(options)
+function parseFilesToFind(options) {
+	let _letterToFileMap = {
+		d: 'deleted',
+		m: 'modified',
+		s: 'staged',
+		u: 'untracked'
+	}
+
+	// defaults
+	let find = {
+		deleted: true,
+		modified: true,
+		staged: true,
+		untracked: true
+	}
+
+	for (let file of argv.except && argv.except.split(',') || []) { // that's a cool line
+		delete find[_letterToFileMap[file[0] && file[0].toLowerCase()]] // another cool line
+	}
+
+	// need to delete all find files if `argv.only` is present
+	find = argv.only ? {} : find
+
+	for (let file of argv.only && argv.only.split(',') || []) {
+		find[_letterToFileMap[file[0].toLowerCase()]] = true
+	}
+
+	options.find = find
+}
